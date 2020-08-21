ae42c449d69bc6c432ef31dac0dc6ad936aa3b8d
andrinagn <64087293+andrinagn@users.noreply.github.com>
2020-08-21 22:44:45 +0200

    21-8-2

---
+++ qgis2web_2020_08_21-18_54_52_331654/data/Area_PN_7.js
@@ -0,0 +1,8 @@
+var json_Area_PN_7 = {
+"type": "FeatureCollection",
+"name": "Area_PN_7",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": null }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -0.920907278738445, 42.428541080131353 ], [ -0.899953833768888, 43.052260922824324 ], [ 0.842562546178961, 43.006978453066331 ], [ 0.804302352253715, 42.384229724440232 ], [ -0.920907278738445, 42.428541080131353 ] ] ] ] } }
+]
+}
