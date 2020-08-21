563679614049cdf8a9fddbd085d02ced1b3ccb42
andrinagn <64087293+andrinagn@users.noreply.github.com>
2020-08-21 23:57:27 +0200

    1

---
+++ qgis2web_2020_08_21-23_55_42_327092/data/AreaEstudio_1.js
@@ -0,0 +1,8 @@
+var json_AreaEstudio_1 = {
+"type": "FeatureCollection",
+"name": "AreaEstudio_1",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "1" }, "geometry": { "type": "MultiPolygon", "coordinates": [ [ [ [ -0.679761561153621, 37.627386462193229 ], [ -0.888272214344301, 37.633626182488214 ], [ -0.88174117812364, 37.851871642861859 ], [ -0.673230524932959, 37.845631922566874 ], [ -0.679761561153621, 37.627386462193229 ] ] ] ] } }
+]
+}
